528f2e726cb7c9ed6ef8307964d7b876ff7bf1b6
fakestarbaby <fakestarbaby@gmail.com>
2012-08-02 10:11:39 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHerokuPostgres::Application.config.secret_token = '93ccc0a75b77c4d69ab6869b7227d656a2665b29a3094dabdd813af233c4c4997a56fa390b1f78949d1285f91d95e672dab3935431412a82491ee6328d4f37a0'
